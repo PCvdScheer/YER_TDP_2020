a2ab1d167a4c793889ba182bfdb72bef4f001fa3
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-02 14:03:01 +0200

    Second commit

--- src/Java_Basics/Exercises/Ex15.java
+++ src/Java_Basics/Exercises/Ex15.java
@@ -1,4 +1,18 @@
 package Java_Basics.Exercises;
 
 public class Ex15 {
+
+    public static void main(String[] args) {
+
+        int temp = 0;
+        for(int i =1; i<21; i++){
+            if(i%2!=0)
+            {temp = temp+i;}
+        }
+
+        System.out.println("The total sum of all integers from 1-20 is: " + temp);
+    }
+
 }
+
+
