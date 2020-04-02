0f95dc67c9e2808629bb4927b47904aa1235ef44
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-02 14:17:58 +0200

    Second commit

--- src/Java_Basics/Exercises/Ex16.java
+++ src/Java_Basics/Exercises/Ex16.java
@@ -1,4 +1,20 @@
 package Java_Basics.Exercises;
 
+import java.util.ArrayList;
+
 public class Ex16 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        for(int i=0; i<10; i++){
+            al.add(1+(i*4));
+        }
+
+        System.out.println("The first ten members from this series are: " + al);
+    }
+
 }
+
+
--- src/Java_Basics/Exercises/Ex19.java
+++ src/Java_Basics/Exercises/Ex19.java
@@ -1,4 +1,28 @@
 package Java_Basics.Exercises;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
 
 public class Ex19 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a word: ");
+        String a = scanner.nextLine();
+
+        System.out.print("What letter would you like to check for: ");
+        char b = scanner.next().charAt(0);
+
+
+        long count = a.chars().filter(ch -> ch == b).count();
+
+        System.out.println("The number of " + b + " in " + a + " equals: " + count);
+
+        }
 }
+
+// leuke test woord: otorhinolaryngological 
+
+
+
