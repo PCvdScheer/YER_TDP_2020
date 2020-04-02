95c731d763366c90b3c263271ebd12ba42f796f1
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-02 14:00:24 +0200

    Second commit

--- src/Java_Basics/Exercises/Ex11.java
+++ src/Java_Basics/Exercises/Ex11.java
@@ -1,4 +1,42 @@
 package Java_Basics.Exercises;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
 
 public class Ex11 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What product would you like to buy: ");
+        int a = scanner.nextInt();
+
+        System.out.print("How many would you like to purchase: ");
+        int b = scanner.nextInt();
+
+        double total = invent(a)*b;
+
+        System.out.println("The total cost of your purchase will be:" + total);
+
+    }
+
+    public static double invent(int product){
+        switch(product){
+            case 1:
+                return 2.95;
+            case 2:
+                return 4.99;
+            case 3:
+                return 5.49;
+            case 4:
+                return 7.80;
+            case 5:
+                return 8.85;
+            default:
+                return 0;
+        }
+
+    }
+
 }
+
--- src/Java_Basics/Exercises/Ex9.java
+++ src/Java_Basics/Exercises/Ex9.java
@@ -1,4 +1,27 @@
 package Java_Basics.Exercises;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
 
 public class Ex9 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> al = new ArrayList<Integer>();
+
+        System.out.print("How many number would you like to enter: ");
+        int runlength = scanner.nextInt();
+
+        for (int i = 0; i < runlength; i++) {
+            System.out.print("Enter a new integer  (" + (i + 1) + "/" + runlength + "):");
+            int a = scanner.nextInt();
+            if(1 <= a && a <= 10){al.add(a);}
+        }
+
+        System.out.println("The number you entered which were between 1-10 are: " + al);
+
+    }
+
 }
+
