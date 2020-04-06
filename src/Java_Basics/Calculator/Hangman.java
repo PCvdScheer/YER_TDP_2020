1d9ad3360f2969d02b18ac6e8780bd9188e08c14
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-06 14:17:32 +0200

    Second commit

--- src/Java_Basics/Calculator/Hangman.java
+++ src/Java_Basics/Calculator/Hangman.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
 import java.util.ArrayList;
-
+import java.util.concurrent.TimeUnit;
 import java.io.*;
 import java.util.Scanner;
 import java.util.Random;
@@ -16,7 +16,7 @@ public class Hangman extends JFrame implements ActionListener {
     JLabel message = new JLabel("MESSAGE PLACEHOLDER");
     String word = "";
     StringBuilder temp;
-    int guesses;
+    int guesses = 6;
     int variable = (int) (Math.random() * 1000);
     JTextField tf1;
 
@@ -26,9 +26,6 @@ public class Hangman extends JFrame implements ActionListener {
 
     Hangman() throws FileNotFoundException {
         JPanel panel = new JPanel();
-
-        this.guesses = 6;
-
         display.setPreferredSize(new Dimension(400, 30));
         display.setHorizontalAlignment(JLabel.CENTER);
         display.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 40));
@@ -48,46 +45,15 @@ public class Hangman extends JFrame implements ActionListener {
         tf1.addActionListener(this);
 
         getContentPane().add(panel, BorderLayout.SOUTH);
-
-        ArrayList<String> dictionary = new ArrayList<String>();
-        File file = new File("D:\\Java\\YER_TDP_2020\\src\\Java_Basics\\Calculator\\Hangman_Wordlist.txt");
-        Scanner sc = new Scanner(file);
-
-        while (sc.hasNextLine())
-            dictionary.add(sc.nextLine());
-
-        Random r = new Random();
-        int index = r.nextInt(dictionary.size());
-        this.word = dictionary.get(index);
-
-        int length = word.length();
-
-        StringBuilder str = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            str.append("*");
-            this.temp = str;
-        }
-        display.setText(temp.toString());
-        message.setText(this.guesses + " guesses left: ");
+        generator();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         Hangman hangman = new Hangman();
-
         hangman.setSize(700, 400);
         hangman.setVisible(true);
         hangman.setDefaultCloseOperation(EXIT_ON_CLOSE);
         System.out.println(hangman.getWord());
-        System.out.println(hangman.temp);
-    }
-
-    public void game(String letter) {
-        if(this.word.contains(letter)){
-            for(int i=0; i < word.length(); i++){
-                if(word.charAt(i) == letter.charAt(0)){temp.setCharAt(i, letter.charAt(0));
-                }
-            }
-        }else{this.setGuesses(this.getGuesses()-1);}
     }
 
     @Override
@@ -99,36 +65,75 @@ public class Hangman extends JFrame implements ActionListener {
         }else{
             game(s1.toLowerCase());
             display.setText(this.temp.toString());
-            message.setText(this.getGuesses() + " guesses left: ");
+            message.setText(this.guesses + " guesses left: ");
         }
 
-        if(!this.getTemp().toString().contains("*")){
+        if(!this.temp.toString().contains("*")){
+            message.setText("You have won the game");
             try {
-                Hangman hangman = new Hangman();
-                display.setText(this.temp.toString());
-                message.setText("You have won the game");
-                System.out.println("You have won the game");
+                TimeUnit.SECONDS.wait(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            try {
+                generator();
             } catch (FileNotFoundException ex) {
                 ex.printStackTrace();
             }
+            System.out.println(this.getWord());
         }
 
-        if(this.getGuesses() == 0){
+        if(this.guesses == 0){
             message.setText("Your lack of vocabulary has killed you!");
+            try {
+                TimeUnit.SECONDS.wait(1);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
+            try {
+                generator();
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(this.getWord());
         }
     }
 
-    public StringBuilder getTemp() {
-        return temp;
+    public void generator() throws FileNotFoundException {
+        ArrayList<String> dictionary = new ArrayList<String>();
+        File file = new File("D:\\Java\\YER_TDP_2020\\src\\Java_Basics\\Calculator\\Hangman_Wordlist.txt");
+        Scanner sc = new Scanner(file);
+        this.guesses =6;
+
+        while (sc.hasNextLine())
+            dictionary.add(sc.nextLine());
+
+        Random r = new Random();
+        int index = r.nextInt(dictionary.size());
+        this.word = dictionary.get(index);
+        int length = word.length();
+
+        temp = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            this.temp.append("*");
+        }
+        display.setText(temp.toString());
+        message.setText(this.guesses + " guesses left: ");
     }
 
-    public int getGuesses() {
-        return guesses;
+    public void game(String letter) {
+        if(this.word.contains(letter)){
+            for(int i=0; i < word.length(); i++){
+                if(word.charAt(i) == letter.charAt(0)){
+                    temp.setCharAt(i, letter.charAt(0));
+                    tf1.setText("");
+                }
+            }
+        }else{this.guesses = this.guesses-1;}
     }
 
-    public void setGuesses(int guesses) {
-        this.guesses = guesses;
-    }
 
 }
 
