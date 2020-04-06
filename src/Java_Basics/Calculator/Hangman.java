2c7ebfd1a48c4f8d1f7aae30f04d9432290b7bd7
SnakeDoctor <23417772+SnakeDocor@users.noreply.github.com>
2020-04-06 11:56:05 +0200

    Second commit

--- src/Java_Basics/Calculator/Hangman.java
+++ src/Java_Basics/Calculator/Hangman.java
@@ -16,7 +16,7 @@ public class Hangman extends JFrame implements ActionListener {
     JLabel message = new JLabel("MESSAGE PLACEHOLDER");
     String word = "";
     StringBuilder temp;
-    int guesses = 6;
+    int guesses;
     int variable = (int) (Math.random() * 1000);
     JTextField tf1;
 
@@ -26,6 +26,8 @@ public class Hangman extends JFrame implements ActionListener {
 
     Hangman() throws FileNotFoundException {
         JPanel panel = new JPanel();
+
+        this.guesses = 6;
 
         display.setPreferredSize(new Dimension(400, 30));
         display.setHorizontalAlignment(JLabel.CENTER);
@@ -63,7 +65,7 @@ public class Hangman extends JFrame implements ActionListener {
 
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            str.append("_ ");
+            str.append("*");
             this.temp = str;
         }
         display.setText(temp.toString());
@@ -78,17 +80,15 @@ public class Hangman extends JFrame implements ActionListener {
         hangman.setDefaultCloseOperation(EXIT_ON_CLOSE);
         System.out.println(hangman.getWord());
         System.out.println(hangman.temp);
-
-
     }
 
-    public String game(String letter) {
+    public void game(String letter) {
         if(this.word.contains(letter)){
-            for(int i=0; i < word.length(); i++);{
-                if(word.charAt(i) == letter){temp.setCharAt(i, letter);}
+            for(int i=0; i < word.length(); i++){
+                if(word.charAt(i) == letter.charAt(0)){temp.setCharAt(i, letter.charAt(0));
+                }
             }
-        }
-        return "";
+        }else{this.setGuesses(this.getGuesses()-1);}
     }
 
     @Override
@@ -96,8 +96,68 @@ public class Hangman extends JFrame implements ActionListener {
         String s1 = (String) tf1.getText();
         if(s1.length() > 1) {
             System.out.println("You can only guess one letter at a time");
-        }else{game(s1.toLowerCase());
+        }else{
+            game(s1.toLowerCase());
+            display.setText(this.temp.toString());
+            message.setText(this.getGuesses() + " guesses left: ");
         }
+
+        if(!this.getTemp().toString().contains("*")){
+            
+            }
+        }
+
+
+    public JLabel getDisplay() {
+        return display;
+    }
+
+    public JLabel getMessage() {
+        return message;
+    }
+
+    public StringBuilder getTemp() {
+        return temp;
+    }
+
+    public int getGuesses() {
+        return guesses;
+    }
+
+    public int getVariable() {
+        return variable;
+    }
+
+    public JTextField getTf1() {
+        return tf1;
+    }
+
+    public void setDisplay(JLabel display) {
+        this.display = display;
+    }
+
+    public void setMessage(JLabel message) {
+        this.message = message;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setTemp(StringBuilder temp) {
+        this.temp = temp;
+    }
+
+    public void setGuesses(int guesses) {
+        this.guesses = guesses;
+    }
+
+    public void setVariable(int variable) {
+        this.variable = variable;
+    }
+
+    public void setTf1(JTextField tf1) {
+        this.tf1 = tf1;
     }
 }
 
